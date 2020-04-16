10ab276889f492bdb307ccb7079fd77067e03901
christianrd <christiandr17@icloud.com>
2020-04-15 21:13:10 -0400

    added project

---
+++ ParkyApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using ParkyApi.Models;
+
+namespace ParkyApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<NationalPark> NationalParks { get; set; }
+    }
+}
---
+++ ParkyApi/Models/NationalPark.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkyApi.Models
+{
+    public class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
