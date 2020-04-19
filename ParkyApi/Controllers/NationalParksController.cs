4b1152eb1f159e04b55947fe05894674c8b15521
christianrd <christiandr17@icloud.com>
2020-04-19 12:06:56 -0400

    Created National Park Api

--- ParkyApi/Controllers/NationalParksController.cs
+++ ParkyApi/Controllers/NationalParksController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ParkyApi.Models;
+using ParkyApi.Models.Dto;
 using ParkyApi.Repository.IRepository;
 
 namespace ParkyApi.Controllers
@@ -26,16 +28,93 @@ namespace ParkyApi.Controllers
         public IActionResult GetNationalParks()
         {
             var objList = this._npRepo.GetNationalParks();
+            var objDto = new List<NationalParkDto>();
 
-            return Ok(objList);
+            foreach( var obj in objList)
+            {
+                objDto.Add(this._mapper.Map<NationalParkDto>(obj));
+            }
+
+            return Ok(objDto);
         }
 
-        [HttpGet(":Id")]
+        [HttpGet("{Id:int}", Name = "GetNationalPark")]
         public IActionResult GetNationalPark(int Id)
         {
             var obj = this._npRepo.GetNationalPark(Id);
+            if ( obj == null)
+            {
+                return NotFound();
+            }
+            var objDto = _mapper.Map<NationalParkDto>(obj);
 
-            return Ok(obj);
+            return Ok(objDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (this._npRepo.NationalParkExists(nationalParkDto.Name))
+            {
+                ModelState.AddModelError("","National Park Exists!");
+                return StatusCode(404, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var nationalParkObj = this._mapper.Map<NationalPark>(nationalParkDto);
+
+            if (!this._npRepo.CreateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when saving the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetNationalPark", new { Id = nationalParkObj.Id }, nationalParkObj);
+        }
+
+        [HttpPatch("{Id:int}", Name = "UpdateNationalPark")]
+        public IActionResult UpdateNationalPark(int Id, [FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null || Id != nationalParkDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var nationalParkObj = this._mapper.Map<NationalPark>(nationalParkDto);
+            if (!this._npRepo.UpdateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when updating the record {nationalParkObj.Name}");
+
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{Id:int}", Name = "DeleteNationalPark")]
+        public IActionResult DeleteNationalPark(int Id)
+        {
+            if (!this._npRepo.NationalParkExists(Id))
+            {
+                return NotFound();
+            }
+
+            var nationalParkObj = this._npRepo.GetNationalPark(Id);
+            if (!this._npRepo.DeleteNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when deleting the record {nationalParkObj.Name}");
+
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
         }
     }
 }
--- ParkyApi/Models/Dtos/NationalParkDto.cs
+++ ParkyApi/Models/Dtos/NationalParkDto.cs
@@ -1,11 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ParkyApi.Models.Dto
 {
     public class NationalParkDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string State { get; set; }
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
